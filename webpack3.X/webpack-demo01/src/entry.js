231fb08b140fdbb6a36cf20c35015e57014d9d1e
Mark <953579842@qq.com>
2018-01-12 00:15:08 +0800

    webpack的图片路径好坑呀

--- webpack3.X/webpack-demo01/src/entry.js
+++ webpack3.X/webpack-demo01/src/entry.js
@@ -1,2 +1,2 @@
  import css from './css/index.css';
- document.getElementById('title').innerHTML = "Hello Ousheng!我是第二个导入的文件热更新";
+ document.getElementById('title').innerHTML = "Hello Ousheng!我是第二个导入的文件热更新再次测试热更新";
